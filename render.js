c0577ed1c13569a35a46dd052997c3b04518e663
dsjramesh7 <rameshdussa07@gmail.com>
2024-03-03 14:38:14 +0530

    project with html and css done

---
+++ render.js
@@ -0,0 +1,7 @@
+import store from "./store.js";
+
+function render() {
+  console.log(store);
+}
+
+export default render;
---
+++ store.js
@@ -0,0 +1,16 @@
+const store = {
+  todos: [
+    {
+      id: "1",
+      title: "Complete Task A",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Read Book",
+      completed: true,
+    },
+  ],
+};
+
+export default store;
